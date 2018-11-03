39be8c9ce33c499d691eda8ac992b57d2d2eb5b9
sgalban <sgalban@seas.upenn.edu>
2018-11-03 16:53:20 -0400

    Removed some extraneous starter code

--- frontend/PreppyClient/App.js
+++ frontend/PreppyClient/App.js
@@ -4,18 +4,11 @@ import Dashboard from './src/components/dashboard/Dashboard';
 import Header, {HeaderButton} from './src/components/Header';
 import {headerStyles} from './src/Styles';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
     render() {
         return (
-            <View style={{backgroundColor: "black"}}>
+            <View>
                 <StatusBar barStyle="light-content"/>
                 <Header style={headerStyles.headerMain} title={"Dashboard"}>
                     <HeaderButton
@@ -31,22 +24,3 @@ export default class App extends Component<Props> {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
