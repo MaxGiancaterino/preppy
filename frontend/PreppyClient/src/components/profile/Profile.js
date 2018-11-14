4c5403fd14151043e2c359806cd92ff80cba376d
sgalban <sgalban@seas.upenn.edu>
2018-11-14 01:13:28 -0500

    Get access to user from profile page
    Also changed the sample user avatar. The other one wasn't doing much for me

--- frontend/PreppyClient/src/components/profile/Profile.js
+++ frontend/PreppyClient/src/components/profile/Profile.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, ScrollView, Button} from 'react-native';
+import {Text, View, ScrollView, Button, Image} from 'react-native';
 import Header, {HeaderButton} from '../Header';
 import {profileStyles} from './ProfileStyles';
 import {headerStyles} from '../../Styles';
@@ -9,19 +9,51 @@ export default class Profile extends Component {
         title: "My Profile",
     };
 
+    componentWillMount() {
+        this.setState({
+            avatar: null,
+            firstName: "",
+            lastName: "",
+            username: "",
+            email: ""
+        });
+        UserData.getUser().then((user) => {
+            this.setState({
+                avatar: user.avatar,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                username: user.username,
+                email: user.email
+            });
+        }).catch((error) => {
+            console.log(error.message);
+        });
+    }
+
     constructor() {
         super();
     }
 
     render() {
         let nav = this.props.navigation;
+        const uriObject = {uri: this.state.avatar}
+
         return(
             <View style={profileStyles.profileMain}>
                 <ScrollView
                     showsVerticalScrollIndicator="false"
-                    style={profileStyles.profileScroll}
+                    contentContainerStyle={profileStyles.profileScroll}
                 >
-                    <Text>This is the profile</Text>
+                    <Image 
+                        source={
+                            this.state.avatar ?
+                            uriObject : 
+                            require("../../../assets/img/profileTemp.png")
+                        }
+                        style={profileStyles.profileAvatar}
+                    />
+                    <Text>{this.state.firstName}</Text>
+                    <Text>{this.state.lastName}</Text>
                 </ScrollView>
             </View>
         );
