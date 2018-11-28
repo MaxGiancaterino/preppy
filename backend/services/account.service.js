e4d7915cab83a82fd940a25502312ab5a7e919bb
Nikolas Alvelo <nalvelo@seas.upenn.edu>
2018-11-28 18:48:10 -0500

    Backend Additions complete

--- backend/services/account.service.js
+++ backend/services/account.service.js
@@ -26,3 +26,49 @@ exports.get = function(uid, next) {
 				next(snapshot.val());
 			});
 }
+
+
+exports.removeRecipe = function(uid, id, next) {
+	if (id === "-1") {
+		return next(null, "Invalid id");
+	} else {
+		database.ref('/users/' + uid + '/cookingQueue')
+				.once('value')
+				.then(function(snapshot) {
+					var queue = snapshot.val();
+					var ix = parseInt(id, 10);
+					var newQueue = queue.filter(rid => rid !== ix);
+					database.ref('/users/' + uid + '/cookingQueue')
+							.set(newQueue)
+							.then(function(res) {
+								next(newQueue);
+							}).catch(function(err) {
+								next(null, err);
+							});
+				});
+	}
+}
+
+exports.addRecipe = function(uid, id, next) {
+	if (id === -1) {
+		return next(null, "Invalid id");
+	}
+	database.ref('/users/' + uid + '/cookingQueue')
+			.once('value')
+			.then(function(snapshot) {
+				var queue = snapshot.val();
+				if (queue.includes(parseInt(id, 10))) {
+					next(null, "Duplicate id");
+				} else {
+					queue.push(parseInt(id, 10));
+					queue.sort();
+					database.ref('/users/' + uid + '/cookingQueue')
+						.set(queue)
+						.then(function(res) {
+							next(queue);
+						}).catch(function(err) {
+							next(null, err);
+						});
+				}
+			});
+}
