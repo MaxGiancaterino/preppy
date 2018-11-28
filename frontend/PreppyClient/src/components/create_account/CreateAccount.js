38a229c9da81be2a0672553bd66d1b899642a122
Regina <reginalin714@gmail.com>
2018-11-28 18:18:32 -0500

    create account page complete

--- frontend/PreppyClient/src/components/create_account/CreateAccount.js
+++ frontend/PreppyClient/src/components/create_account/CreateAccount.js
@@ -1,27 +1,98 @@
 import React, {Component} from 'react';
 import {Image, Stylesheet, Text, TextInput, View, ScrollView, Button, TouchableOpacity} from 'react-native';
 import Header, {HeaderButton} from '../Header';
-import {pantryStyles} from './CreateAccountStyles';
+import {createAccountStyles} from './CreateAccountStyles';
 import {headerStyles} from '../../Styles';
 
 export default class CreateAccount extends Component {
 
     static navigationOptions = {
         title: "CreateAccount",
+        header: null,
     };
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             username: "",
-            password: ""
+            phoneNumber: 0,
+            password: "",
+            displayName: "",
+            //photoUrl: String,
         }
     }
 
-    render() {
-        return(
-            <View style={CreateAccountStyles.createAccountMain}>
+    navigateToLogin = () => {
+        this.props.navigation.navigate("Login")
+    }
 
+    create_account() {
+        // TO DO: create the account 
+        //this.props.navigation.navigate("CreateAccount");
+    }
+
+    render() {
+
+        return(
+            <View style={createAccountStyles.createAccountMain}>
+
+
+                <View style={createAccountStyles.formContainer}>
+                    <View>
+                        <Text style={createAccountStyles.appName}>
+                            Preppy {'\n'}
+                            <Text style={createAccountStyles.description}>
+                            {'\n'}
+                            Enter info to create a new account.
+                            </Text>
+                        </Text>
+                    </View>
+
+                    <View style={createAccountStyles.inputBox}>
+                        <TextInput style={createAccountStyles.input}
+                            onChangeText={(username) => this.setState({email})}
+                            placeholder='Email'
+                        />
+                    </View>
+
+                    <View style={createAccountStyles.inputBox}>
+                        <TextInput style={createAccountStyles.input}
+                            onChangeText={(password) => this.setState({password})}
+                            placeholder='Password'
+                        />
+                    </View>
+
+                    <View style={createAccountStyles.inputBox}>
+                        <TextInput style={createAccountStyles.input}
+                            onChangeText={(username) => this.setState({phoneNumber})}
+                            placeholder='Phone number'
+                        />
+                    </View>
+
+                    <View style={createAccountStyles.inputBox}>
+                        <TextInput style={createAccountStyles.input}
+                            onChangeText={(username) => this.setState({displayName})}
+                            placeholder='Full name'
+                        />
+                    </View>
+
+                    <View style={createAccountStyles.createAccountButton}>
+                        <Button
+                            onPress={() => {this.create_account()}}
+                            title="Create Account"
+                            color="#FFFFFF"
+                        />
+                    </View>
+                    
+                    <View style={createAccountStyles.backButton}>
+                        <Button
+                            onPress={() => {this.navigateToLogin()}}
+                            title="Back to Login"
+                            color = "lightgray"
+                        />
+                    </View>
+
+                </View>
             </View>
         );
     }
