2631185a7e6ba5788e3c2918c4d97d84430deb11
Chris Urffer <c.urffer@live.com>
2019-04-01 16:40:37 -0400

    added a lot more recipes

--- recipe_utils/Executables/parser_run.bat
+++ recipe_utils/Executables/parser_run.bat
@@ -1,5 +1,5 @@
 :: parse existing recipes
 ::java -jar parser.jar (numThreads) [startingIndex] [endingIndex]
-::java -jar parser.jar 8 215827 220000
-java -jar parser.jar 8 15000
+java -jar parser.jar 2 48600 100120
+::java -jar parser.jar 8 15000
 pause
--- recipe_utils/Executables/scraper_run.bat
+++ recipe_utils/Executables/scraper_run.bat
@@ -1,5 +1,5 @@
 :: scrape new recipes
 :: set (numToDownload) to negative to download indefinitely
 :: java -jar scraper.jar (startingIndex) (numToDownload)
-java -jar scraper.jar 028026 -1
+java -jar scraper.jar 058218 -1
 pause
